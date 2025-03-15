f2671464bfb2845b96d4a44f8ec6c0399f36abe5
zhangsonglin <18829581076@163.com>
2025-03-15 21:07:54 +0800

    update some

--- llm_api/deepseek/deepseek_api.py
+++ llm_api/deepseek/deepseek_api.py
@@ -2,7 +2,7 @@
 
 from openai import OpenAI
 
-client = OpenAI(api_key="sk-de3d39dbb82e4d1b9635412451a2b97b", base_url="https://api.deepseek.com")
+client = OpenAI(api_key="sk-de3d39dbb82e4d1b9635412451a2b97b-1", base_url="https://api.deepseek.com")
 text = '今天星期几'
 response = client.chat.completions.create(
     model="deepseek-chat",
